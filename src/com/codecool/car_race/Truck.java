e26696414131e2402fe487dcc8605ff871713508
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-12-06 14:10:57 +0100

    created Truck class & implemented setName, prepareForLap

---
+++ src/com/codecool/car_race/Truck.java
@@ -0,0 +1,23 @@
+package com.codecool.car_race;
+
+public class Truck extends Vehicle {
+
+    private int breakdownTurnsLeft;
+
+    protected void setName() {
+        name = Integer.toString(Util.getRandomNumber(0, 1000));
+    }
+
+    public void prepareForLap(Race race) {
+        if (breakdownTurnsLeft > 0) {
+            currentSpeed = 0; // TODO: this case should be handled in MoveForAnHour
+            breakdownTurnsLeft --;
+        } else {
+            if (Util.prob(0.05)) {
+                race.anyBrokenTrucks = true;
+                breakdownTurnsLeft = 2;
+            }
+        }
+
+    }
+}
