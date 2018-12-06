e8a64f5bd28de7087632d6212f1364c59f2da1ef
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-12-06 11:31:43 +0100

    created Weather class

--- src/com/codecool/car_race/Race.java
+++ src/com/codecool/car_race/Race.java
@@ -2,4 +2,12 @@ package com.codecool.car_race;
 
 public class Race {
 
+    public void simulateRace() {
+
+    }
+
+    public void printRaceResults() {
+
+    }
+
 }
---
+++ src/com/codecool/car_race/Weather.java
@@ -0,0 +1,14 @@
+package com.codecool.car_race;
+
+public class Weather {
+
+    private boolean raining;
+
+    public void setRaining() {
+
+    }
+
+    public boolean isRaining() {
+        return raining;
+    }
+}
