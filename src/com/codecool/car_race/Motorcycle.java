989e3e7efde7bc7302ba4133a1ccc7270f68dd41
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-12-07 10:02:52 +0100

    implemented Motorcycle class

--- src/com/codecool/car_race/Motorcycle.java
+++ src/com/codecool/car_race/Motorcycle.java
@@ -1,13 +1,30 @@
 package com.codecool.car_race;
 
 public class Motorcycle extends Vehicle {
+
+    private static final int SPEED = 100;
+    private static final int RAIN_DISADVANTAGE_MIN = 5;
+    private static final int RAIN_DISADVANTAGE_MAX = 50;
+
+    public Motorcycle(Race race) {
+        setName(race);
+    }
+
     @Override
     protected void setName() {
+        // TODO: I need parameters in this class only, so is it okay to overload this method with one with parameters?
+    }
 
+    protected void setName(Race race) {
+        name = "Motorcycle " + race.motorcycles.size();
     }
 
     @Override
     public void prepareForLap(Race race) {
-
+        if (race.weather.isRaining()) {
+            currentSpeed = SPEED - Util.getRandomNumber(RAIN_DISADVANTAGE_MIN, RAIN_DISADVANTAGE_MAX);
+        } else {
+            currentSpeed = SPEED;
+        }
     }
 }
--- src/com/codecool/car_race/Race.java
+++ src/com/codecool/car_race/Race.java
@@ -1,10 +1,15 @@
 package com.codecool.car_race;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Race {
 
     public boolean anyBrokenTrucks;
 
     public Weather weather;
+
+    public List<Motorcycle> motorcycles = new ArrayList<>();
 
     public Race() {
         weather = new Weather();
